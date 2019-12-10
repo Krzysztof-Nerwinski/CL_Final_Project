b69ca862d440db447c4500908ec32cb152ec4ddd
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2019-12-10 10:44:49 +0100

    model methods extended

--- timer/models.py
+++ timer/models.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -13,10 +11,17 @@ class Client(models.Model):
     phone = models.CharField(max_length=16)
     tax_no = models.CharField(max_length=32)
 
+    def __str__(self):
+        return f"Client name: {self.name}"
+
+
 
 class Task(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
+
+    def __str__(self):
+        return f"Task name: {self.name}, description: {self.description}"
 
 
 class Case(models.Model):
@@ -26,11 +31,17 @@ class Case(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     tasks = models.ManyToManyField(Task, through="CaseTasks")
 
+    def __str__(self):
+        return f"Case {self.name} for client: {self.client}. Description: {self.description}"
+
 
 class CaseTasks(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     case = models.ForeignKey(Case, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"Price per hour for {self.case} - {self.task} is {self.price}"
 
 
 class Timer(models.Model):
@@ -41,6 +52,11 @@ class Timer(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     case = models.ForeignKey(Case, on_delete=models.CASCADE)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Timer no {self.id}, started on {self.start_time}, finished on {self.end_time}," \
+               f"duration {self.duration}, for  {self.client}, by {self.employee} on case {self.case}" \
+               f"and task {self.task}"
 
     @property
     def duration(self):
